59e28a69c40564e62411657be908fa29c4dba035
Tony Arnold <tony@thecocoabots.com>
2020-07-01 12:00:41 +1000

    Define a Swift package

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "Peertalk",
+    products: [
+        .library(name: "Peertalk", targets: ["Peertalk"]),
+    ],
+    targets: [
+        .target(
+            name: "Peertalk",
+            path: "peertalk",
+            exclude: [
+                "Info.plist",
+                "Peertalk.h",
+                "prefix.pch",
+                "PTPrivate.h"
+            ],
+            publicHeadersPath: "."
+        ),
+        .testTarget(
+            name: "PeertalkTests",
+            dependencies: ["Peertalk"],
+            path: "peertalk-tests",
+            exclude: [
+                "en.lproj",
+                "peertalkTests-Info.plist"
+            ]
+        )
+    ]
+)
