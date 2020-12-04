6b624612fd14dd63c0c2ae75a0ace05f725b8060
Jonathan Dann <jonathandann@users.noreply.github.com>
2020-12-04 13:23:47 +0000

    [Consistency] Remove comment header from Peertalk.h
    None of the other files have this, so now it's removed.

--- peertalk/Peertalk.h
+++ peertalk/Peertalk.h
@@ -1,11 +1,3 @@
-//
-//  Peertalk.h
-//  Peertalk
-//
-//  Created by Marek Cirkos on 12/04/2016.
-//
-//
-
 #import <Foundation/Foundation.h>
 
 //! Project version number for Peertalk.
@@ -15,7 +7,6 @@ FOUNDATION_EXPORT double PeertalkVersionNumber;
 FOUNDATION_EXPORT const unsigned char PeertalkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Peertalk/PublicHeader.h>
-
 
 #import <Peertalk/PTChannel.h>
 #import <Peertalk/PTProtocol.h>
