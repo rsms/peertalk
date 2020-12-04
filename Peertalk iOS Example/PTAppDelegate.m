07253ef2bce9332d44e63dba4221a1a9649c4dfe
Jonathan Dann <jonathandann@users.noreply.github.com>
2020-12-04 13:15:37 +0000

    [iOS Example] Remove deprecated method call
    `PTAppDelegate` doesn't need to set the status bar style anymore.
    
    Removes a deprecation warning.

--- Peertalk iOS Example/PTAppDelegate.m
+++ Peertalk iOS Example/PTAppDelegate.m
@@ -7,15 +7,4 @@
 
 @synthesize window = window_;
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackOpaque animated:NO];
-  return YES;
-}
-
-
-//- (void)applicationWillTerminate:(UIApplication *)application {
-//}
-
-
 @end
