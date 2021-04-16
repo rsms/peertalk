29d0a3ab3a92c6d4e53b327f8d2eba0012778a99
Casper Jeukendrup <cbjeukendrup@users.noreply.github.com>
2021-04-16 17:49:09 +0200

    Update Project Settings and Lower Deployment Targets

--- Peertalk Swift Example/SceneDelegate.swift
+++ Peertalk Swift Example/SceneDelegate.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	var window: UIWindow?
-
 
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		// Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -46,7 +46,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		// Use this method to save data, release shared resources, and store enough scene-specific state information
 		// to restore the scene back to its current state.
 	}
-
-
 }
 
